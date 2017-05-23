36e5624b1330302850c97f0d8ee03cd749ec2512
KathrinDavis <kathrinjdavis@gmail.com>
2017-05-22 22:31:22 -0700

    no nodes

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-	port: 3306,
-	user:"localhost",
-	user: "root",
-	password:"Code2017",
-	database:"burgers_db"
-});
+var connection;
 
-connection.connect(function(err){
-	if(err){
-		console.error("error connecting: "+ err.stack);
-		return;
-	}
-	console.log("connected as id "+ connection.threadId);
-});
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+		connection = mysql.createConnection({
+		user:"localhost",
+		user: "root",
+		password:"Code2017",
+		database:"burgers_db"
+	});
+};
 
+
+connection.connect();
 module.exports = connection;
